c3a0d43c9275468acde4ae88dc0be2ad6086abfb
malktbi <109072271+malktbi@users.noreply.github.com>
2022-07-20 11:50:26 +0300

    Added more tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -8,18 +8,18 @@ class servertests(unittest.TestCase):
 
     def test_initialize_server(self): 
         actual = server.initialize_server("localhost", 80,1)
-        expected = ""
+        expected = "Listening on localhost:80"
         self.assertEqual(actual, expected)
 
     def test_receive_config(self):
         actual = server.receive_config(socket.socket,"localhost",3)
-        expected = ""
+        expected = "Connected by localhost"
         self.assertEqual(actual, expected)
 
     def test_receive_data(self):
         actual = server.receive_data(socket.socket, 3)
-        expected = ""
-        self.assertEqual(actual, expected)
+        notExpected = "Connection Error on receiving data."
+        self.assertNotEqual(actual, notExpected)
 
     def test_send_response(self):
         actual = server.send_response(socket.socket, "hello", 3)
@@ -28,17 +28,17 @@ class servertests(unittest.TestCase):
 
     def test_get_private_key(self):
         actual = server.get_private_key(3,"12345678fdfsd")
-        expected = ""
+        expected = "12345678fdfsd"
         self.assertEqual(actual, expected)
 
     def test_print_dict(self):
-        actual = server.print_dict({one}:{1}))
-        expected = ""
+        actual = server.print_dict("{one}:{1}")
+        expected = "------------Start Dictionary Text Output------------\n{one}:{1}\n------------End Dictionary Text Output------------"
         self.assertEqual(actual, expected)
 
     def test_print_to_terminal(self):
         actual = server.print_to_terminal("testing testing")
-        expected = ""
+        expected = "------------Start Output------------\ntesting testing\n------------End Output------------"
         self.assertEqual(actual, expected)
 
     def test_process_recv_data(self):
@@ -46,7 +46,7 @@ class servertests(unittest.TestCase):
                         recv_data: bytes,
                         server_configuration: dict,
                         priv_key: rsa.PrivateKey = EXAMPLE_PRIV_KEY)
-        expected = ""
+        expected = "'DATA_OK"
         self.assertEqual(actual, expected)
 
     def test_start_server(self):
