04e698ba4473affce97e0bd7c3e7cfc675707581
Zhu Lin Ch'ng <z.chng@liverpool.ac.uk>
2022-07-21 21:27:15 +0800

    test: added client unittest working draft

--- tests/client_test.py
+++ tests/client_test.py
@@ -5,20 +5,22 @@ import pickle
 import unittest
 from unittest import mock
 from io import StringIO
+import json
 import os
 from os.path import dirname, join, abspath
 import rsa
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
-from tests import client_testcase
-from encryption import EXAMPLE_PUB_KEY, EXAMPLE_PRIV_KEY
 from cs_network import client
+from encryption import EXAMPLE_PUB_KEY, EXAMPLE_PRIV_KEY
+from tests import client_testcase
+
 
 class TestClient(unittest.TestCase):
     """Unit test for client."""
 
     @mock.patch('sys.stdout', new_callable=StringIO)
-    def client_input_test(self, test_inputs: list, mock_stdout: StringIO) -> str:
-        """Test input_data."""
+    def client_input_test(self, test_inputs: list, mock_stdout: StringIO) -> tuple:
+        """Mock input_data."""
         outputs = []
         test_inputs = [str(x) for x in test_inputs]
         with mock.patch('builtins.input', side_effect=test_inputs):
@@ -39,7 +41,7 @@ class TestClient(unittest.TestCase):
                 del config_dict[key]
         return config_dict
 
-    def test_input_1(self):
+    def test_input_encrypted_dict(self):
         """Encrypted dictionary input test case."""
 
         config, data, stdout = self.client_input_test(client_testcase.input_1)
@@ -49,40 +51,21 @@ class TestClient(unittest.TestCase):
         self.assertEqual(config, client_testcase.input_1_config)
         self.assertEqual(data, client_testcase.input_1_data)
 
-    def test_input_2(self):
+    def test_input_plain_dict_serial(self):
         """
         Plain dictionary input test case.
-        Using binary serialization.
+        Testing binary, JSON and XML serialization.
         """
+        tests = [client_testcase.test_case_2,
+                 client_testcase.test_case_3, client_testcase.test_case_4]
+        for test in tests:
+            with self.subTest(case=test['case']):
+                config, data, stdout = self.client_input_test(test['input'])
+                config = self.del_none_from_dict(config)
+                self.assertEqual(config, test['input_config'])
+                self.assertEqual(data, test['input_data'])
 
-        config, data, stdout = self.client_input_test(client_testcase.input_2)
-        config = self.del_none_from_dict(config)
-        self.assertEqual(config, client_testcase.input_2_config)
-        self.assertEqual(data, client_testcase.input_2_data)
-
-    def test_input_3(self):
-        """
-        Plain dictionary input test case.
-        Using JSON serialization.
-        """
-
-        config, data, stdout = self.client_input_test(client_testcase.input_3)
-        config = self.del_none_from_dict(config)
-        self.assertEqual(config, client_testcase.input_3_config)
-        self.assertEqual(data, client_testcase.input_3_data)
-
-    def test_input_4(self):
-        """
-        Plain dictionary input test case.
-        Using XML serialization.
-        """
-
-        config, data, stdout = self.client_input_test(client_testcase.input_4)
-        config = self.del_none_from_dict(config)
-        self.assertEqual(config, client_testcase.input_4_config)
-        self.assertEqual(data, client_testcase.input_4_data)
-
-    def test_input_5(self):
+    def test_input_encrypted_text(self):
         """Encrypted text test case."""
 
         config, data, stdout = self.client_input_test(client_testcase.input_5)
@@ -95,7 +78,7 @@ class TestClient(unittest.TestCase):
         self.assertEqual(config, client_testcase.input_5_config)
         self.assertEqual(data, client_testcase.INPUT_5_DATA)
 
-    def test_input_6(self):
+    def test_input_plain_text(self):
         """Plain text test case."""
 
         config, data, stdout = self.client_input_test(client_testcase.input_6)
@@ -116,20 +99,22 @@ class TestClient(unittest.TestCase):
         client_testcase.test_case_5['input_config']['public_key'] = EXAMPLE_PUB_KEY
         client_testcase.test_case_5['input_config']['serialize'] = None
 
-        tests = [client_testcase.test_case_1, client_testcase.test_case_5]
+        tests = [client_testcase.test_case_1, client_testcase.test_case_2,
+                 client_testcase.test_case_3, client_testcase.test_case_4,
+                 client_testcase.test_case_5, client_testcase.test_case_6]
         for test in tests:
             with self.subTest(case=test['case']):
                 # As encryption uses random numbers for padding, we need to
                 # decrypt the data to compare it to the original.
                 if test['input_config']['encrypt'] == 1:
                     test_config, test_data = client.process_data(test['input_config'],
-                                                                test['input_data'])
+                                                                 test['input_data'])
                     self.assertEqual(test_config, test['output_config'])
                     if test['input_config']['type'] == 1:
                         self.assertEqual(
                             (rsa.decrypt(
                                 pickle.loads(test_data), EXAMPLE_PRIV_KEY)).decode('utf-8'),
-                                        str(test['input_data']))
+                            str(test['input_data']))
                     elif test['input_config']['type'] == 2:
                         # delete file after test
                         try:
@@ -140,26 +125,50 @@ class TestClient(unittest.TestCase):
                             pass
                         self.assertEqual((rsa.decrypt(test_data, EXAMPLE_PRIV_KEY)).decode('utf-8'),
                                          str(test['input_data']))
-                # elif test['input_config']['encrypt'] == 2:
-                #     test_config, test_data = client.process_data(test['input_config'],
-                #                                                 test['input_data'])
-                #     self.assertEqual(test_config, test['output_config'])
-                #     if test['input_config']['type'] == 1:
-                #         self.assertEqual(pickle.loads(test_data), test['input_data'])
-                #     elif test['input_config']['type'] == 2:
-                #         # delete file after test
-                #         os.remove(f"{test['input_config']['txtfilepath']}_{time.strftime('%Y%m%d_%H%M%S', time.localtime())}.txt")
-                #         self.assertEqual(test_data, test['input_data'])
+                elif test['input_config']['encrypt'] == 2:
+                    test_config, test_data = client.process_data(test['input_config'],
+                                                                 test['input_data'])
+                    self.assertEqual(test_config, test['output_config'])
+                    if test['input_config']['type'] == 1 and test['input_config']['serialize'] == 1:
+                        self.assertEqual(
+                            test_data, pickle.dumps(test['input_data']))
+                    elif test['input_config']['type'] == 1 and test[
+                            'input_config']['serialize'] == 2:
+                        self.assertEqual(test_data, json.dumps(
+                            test['input_data']).encode('utf-8'))
+                    elif test['input_config']['type'] == 1 and test[
+                            'input_config']['serialize'] == 3:
+                        self.assertEqual(
+                            test_data, test['output_data'].encode('utf-8'))
+                    elif test['input_config']['type'] == 2:
+                        # delete file after test
+                        try:
+                            os.remove(f"\
+{test['input_config']['txtfilepath']}_\
+{time.strftime('%Y%m%d_%H%M%S', time.localtime())}.txt")
+                        except FileNotFoundError:
+                            pass
+                        self.assertEqual(
+                            test_data, test['input_data'].encode('utf-8'))
 
-    #     send_config, encoded_data = client.process_data(config, data)
-    #     print(send_config, encoded_data)
-        # self.assertEqual(send_config, testcase_config)
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def continue_input_test(self, test_inputs: list, mock_stdout: StringIO) -> tuple:
+        """Mock for continue input."""
+        outputs = []
+        with mock.patch('builtins.input', side_effect=test_inputs):
+            start_point = client.continue_input()
+            outputs.append(mock_stdout.getvalue())
+        return start_point, outputs[0].split("\n")
 
-    # def test_continue(self):
-    #     """Test for continue input."""
-    #     with mock.patch('builtins.input', side_effect=["y", "n"]):
-    #         self.assertTrue(client.continue_input())
-    #         self.assertFalse(client.continue_input())
+    def test_continue(self):
+        """Test for continue_input."""
+        tests = [client_testcase.test_case_7, client_testcase.test_case_8,
+                 client_testcase.test_case_9, client_testcase.test_case_10]
+        for test in tests:
+            with self.subTest(case=test['case']):
+                start_point, stdout = self.continue_input_test(
+                    test['input_data'])
+                self.assertEqual(start_point, test['output_data'])
 
 
 if __name__ == "__main__":
--- tests/client_testcase.py
+++ tests/client_testcase.py
@@ -1,6 +1,7 @@
 """Test cases for client."""
 import os
 
+
 def split_dict(data_dict: dict) -> list:
     """
     Convert key values of a dictionary to list elements.
@@ -24,8 +25,6 @@ def join_config_data(config_dict: dict, data_dict: dict) -> list:
     :return: The list of values in the dictionaries."""
     return list(config_dict.values()) + split_dict(data_dict)
 
-# Input data test cases
-
 
 # Encrypted dictionary test case
 input_1_config = {"type": 1, "encrypt": 1, "public_key": ""}
@@ -33,7 +32,7 @@ input_1_data = {'encryptedkey': 'encryptedvalue',
                 '123': '456', 'nested_dict': {'nest': 'value'}}
 input_1 = join_config_data(input_1_config, input_1_data) + [""]
 output_1_config = {"type": 1, "encrypt": 1, "serialize": 1}
-test_case_1 = {'case': 'Encrypted Dictionary','input_config': input_1_config,
+test_case_1 = {'case': 'Encrypted Dictionary', 'input_config': input_1_config,
                'input_data': input_1_data,
                'input': input_1, 'output_config': output_1_config}
 
@@ -42,18 +41,38 @@ input_2_config = {"type": 1, "encrypt": 2, "serialize": 1}
 input_2_data = {'testkey': 'testvalue',
                 '123': '456', 'nested_dict': {'nest': 'value'}}
 input_2 = join_config_data(input_2_config, input_2_data) + [""]
+output_2_config = {"type": 1, "encrypt": 2, "serialize": 1}
+test_case_2 = {'case': 'Plain Dictionary - Binary Serialization',
+               'input_config': input_2_config,
+               'input_data': input_2_data,
+               'input': input_2,
+               'output_config': output_2_config}
 
 # Plain dictionary test case - JSON serialization
 input_3_config = {"type": 1, "encrypt": 2, "serialize": 2}
 input_3_data = {'testkey': 'testvalue',
                 '123': '456', 'nested_dict': {'nest': 'value'}}
 input_3 = join_config_data(input_3_config, input_3_data) + [""]
+output_3_config = {"type": 1, "encrypt": 2, "serialize": 2}
+test_case_3 = {'case': 'Plain Dictionary - JSON Serialization',
+               'input_config': input_3_config,
+               'input_data': input_3_data,
+               'input': input_3,
+               'output_config': output_3_config}
 
 # Plain dictionary test case - XML serialization
 input_4_config = {"type": 1, "encrypt": 2, "serialize": 3}
 input_4_data = {'testkey': 'testvalue',
                 'a123': '456', 'nested_dict': {'nest': {'nest2': 'value'}}}
 input_4 = join_config_data(input_4_config, input_4_data) + [""]
+output_4_config = {"type": 1, "encrypt": 2, "serialize": 3}
+OUTPUT_4_DATA = '<root><testkey>testvalue</testkey><a123>456</a123>\
+<nested_dict><nest><nest2>value</nest2></nest></nested_dict></root>'
+test_case_4 = {'case': 'Plain Dictionary - XML Serialization',
+               'input_config': input_4_config,
+               'input_data': input_4_data,
+               'input': input_4, 'output_data': OUTPUT_4_DATA,
+               'output_config': output_4_config}
 
 # Encrypted text test case
 input_5_config = {"type": 2, "txtfilepath": os.path.dirname(__file__),
@@ -72,3 +91,22 @@ input_6_config = {"type": 2, "txtfilepath": os.path.dirname(__file__),
                   "txtfilename": 'client_output', "encrypt": 2}
 INPUT_6_DATA = "plaintext"
 input_6 = join_config_data(input_6_config, {}) + [INPUT_6_DATA] + [""]
+output_6_config = {"type": 2, "encrypt": 2,
+                   "public_key": None, "serialize": None}
+test_case_6 = {'case': 'Plain Text - No Serialization',
+               'input_config': input_6_config,
+               'input_data': INPUT_6_DATA,
+               'input': input_6,
+               'output_config': output_6_config}
+
+test_case_7 = {'case': 'Continue from input config', 'input_data': ['y','1'],
+                'output_data': 1}
+
+test_case_8 = {'case': 'Continue from input data', 'input_data': ['y','2'],
+               'output_data': 2}
+
+test_case_9 = {'case': 'Exit', 'input_data': ['y', '3'],
+               'output_data': 0}
+
+test_case_10 = {'case': 'Exit', 'input_data': 'n',
+               'output_data': 0}
